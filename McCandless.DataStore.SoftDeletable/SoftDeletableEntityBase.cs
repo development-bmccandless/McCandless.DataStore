f905cc670200b5935d8a30caabf39f2cfbd2723b
bmccandless <development.bmccandless@gmail.com>
2025-11-04 11:14:12 -0500

    Set serialization properties on entity base classes

--- McCandless.DataStore.SoftDeletable/SoftDeletableEntityBase.cs
+++ McCandless.DataStore.SoftDeletable/SoftDeletableEntityBase.cs
@@ -1,13 +1,20 @@
 ﻿namespace McCandless.DataStore.SoftDeletable
 {
     using System;
+    using System.Text.Json.Serialization;
 
     public abstract class SoftDeletableEntityBase<TIdentity> : EntityBase<TIdentity>
     {
+        [JsonPropertyName("deletedBy")]
+        [JsonPropertyOrder(994)]
         public string? DeletedBy { get; set; }
 
+        [JsonPropertyName("deletedDateTime")]
+        [JsonPropertyOrder(995)]
         public DateTime? DeletedDateTime { get; set; }
 
+        [JsonPropertyName("isDeleted")]
+        [JsonPropertyOrder(996)]
         public bool? IsDeleted { get; set; }
     }
 }
--- McCandless.DataStore/EntityBase.cs
+++ McCandless.DataStore/EntityBase.cs
@@ -1,17 +1,28 @@
 ﻿namespace McCandless.DataStore
 {
     using System;
+    using System.Text.Json.Serialization;
 
     public abstract class EntityBase<TIdentity>
     {
+        [JsonPropertyName("id")]
+        [JsonPropertyOrder(0)]
         public string? Id { get; set; }
 
+        [JsonPropertyName("createdBy")]
+        [JsonPropertyOrder(990)]
         public string? CreatedBy { get; set; }
 
+        [JsonPropertyName("createdDateTime")]
+        [JsonPropertyOrder(991)]
         public DateTime? CreatedDateTime { get; set; }
 
+        [JsonPropertyName("updatedBy")]
+        [JsonPropertyOrder(992)]
         public string? UpdatedBy { get; set; }
 
+        [JsonPropertyName("updatedDateTime")]
+        [JsonPropertyOrder(993)]
         public DateTime? UpdatedDateTime { get; set; }
 
         public abstract TIdentity GetIdentity();
